87911bf3a902d52e3bd06370ac41c90ddc6b1236
tvoloshin <tvoloshin38@gmail.com>
2023-09-05 16:10:07 +0300

    Add time library and x-axis limit

--- visualizer/src/App.js
+++ visualizer/src/App.js
@@ -8,9 +8,11 @@ import Switch from "react-switch";
 
 import { properties } from './properties.js';
 
+import 'chartjs-adapter-date-fns';
+
 function Temperature({ value }) {
     const color = "hsl(" + 60 * (1 - (value - 300) / (1000 - 300)) + " 100% 50%)"
-    console.log(color)
+//    console.log(color)
 
     return (
         <div className="temp-field" style={{background: color}}>
@@ -38,13 +40,21 @@ function App() {
 
     const switchOn = points.length === 0 ? false : !!points[points.length - 1].state;
     const temperature = points.length === 0 ? 300 : points[points.length - 1].temp;
+//    const minX = points.length === 0 ? Date.now() : points[points.length - 1].time - 60e3;
+    const minX = points.length === 0 ? 0 : points[points.length - 1].time - 60;
 
     useEffect(() => {
         const source = new EventSource(properties.baseUrl + properties.eventsPath);
 
+        let startTime;
         source.onmessage = (e) => {
-            const newPoint = JSON.parse(e.data);
+            startTime = startTime || Date.now();
+
+            let newPoint = JSON.parse(e.data);
+//            newPoint.time = startTime + newPoint.time * 1e3;
+
             console.log(newPoint);
+
             setPoints(points => [...points, newPoint]);
         };
 
@@ -56,7 +66,20 @@ function App() {
     return (
         <div style={{display: "flex", alignItems: "center"}}>
             <div style={{width: "800px"}}>
-                <Line data={data}/>
+                <Line data={data} options={{
+                                            animation: {
+                                                    duration: 0
+                                                },
+                                            scales: {
+                                              x: {
+                                                  /*type: 'time',
+                                                  time: {
+                                                      unit: 'second'
+                                                  },*/
+                                                  min: minX,
+                                              }
+                                            }
+                                            }}/>
             </div>
             <div>
                 <Switch
